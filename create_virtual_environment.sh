abd8cff053e373bd64d99d37cfa6451974ec0e93
Ricardo Fantin da Costa <ricardofantin@gmail.com>
2020-07-29 23:13:39 -0300

    create_environment.sh try to active environment another way
    create_environment.sh try to activate environment to install packages
    with pip in the environment. It was not working.
    .gitignore can ignore files created by boletim_Paranaguá/parse_paranagua.py .

--- create_virtual_environment.sh
+++ create_virtual_environment.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-virtualenv -p python3 enviroment
-chmod +x ./enviroment/bin/activate
-source enviroment/bin/activate
+virtualenv -p python3 environment
+chmod +x ./environment/bin/activate ./environment/bin/activate_this.py
+source environment/bin/activate
+source environment/bin/activate_this.py
 pip3 install pandas
