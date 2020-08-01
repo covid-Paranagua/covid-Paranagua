302690469f0a36718ed15e8912630f4703482407
Ricardo Fantin da Costa <ricardofantin@gmail.com>
2020-08-01 18:22:24 -0300

    Convert new PDF files in a batch
    PDF files gotten from Paranaguá website now are converted to .csv
    after downloaded automatically.
    boletim_Paranaguá/README.md explains PDFs expected structure and at
    the same time the CSV output.
    .gitignore had environment/ folder included.

--- boletim_Paranaguá/README.md
+++ boletim_Paranaguá/README.md
@@ -1,5 +1,40 @@
 # Boletim Paranaguá
 
-The script download data from Paranaguá Counsil Website and convert the pdf to csv to be easier to work from machine. The csv are used to plot maps in script .... Website to download is http://www.paranagua.pr.gov.br/boletim-epidemiologico.php .
+The script download data from Paranaguá Counsil Website and convert the pdf to csv to be easier to machine read. PDF's website source is http://www.paranagua.pr.gov.br/boletim-epidemiologico.php . The csv are intended to be used to plot maps.
 
-Still testing better approach. Need to test pypdf4, tabula-py and camelot.
+Three libraries to process PDF were tested: pypdf4, tabula-py and camelot. The latter one was the best because is intended to read tables and can read portuguese accents in characters.
+
+## PDF Structure
+
+From 02-06-2020 until 19-07-2020 the PDFs have four groups of page displaying tables that we call T1, T2, T3 and T4. Beginning in 20-07-2020 until recently the PDFs have only the firsts three table types.
+
+### T1
+Show number of confirmed cases by neighbourhood. It is identified by a string ending with "CASOS CONFIRMADOS POR BAIRRO".
+
+|Bairro|Feminino|Masculino|Total|
+
+### T2
+Show tests results. Inform how many are waiting results, how many were tested negative for COVID-19 and how many have healed.
+
+ * Waiting results:
+ * * d[2][1] "Feminino - NN"
+ * * d[2][2] "Masculino - NN"
+ * Tested Negative:
+ * * d[1][5] "Masculino - NN"
+ * * d[1][6] "Feminino - NN"
+ * Healed:
+ * * d[2][5] "Masculino - NN"
+ * * d[2][6] "Feminino - NN"
+
+### T3
+Number of deaths. It is identifyied by a string ending with "ÓBITOS".
+
+|Bairro|Idade|Sexo|
+
+### T4
+Number of confirmed tests for each neighbourhood. It is identifyied by a string ending with "CASOS CONFIRMADOS POR BAIRRO E IDADE". This table is not available anymore in 20-07-2020 onward.
+
+|Confirmados|Bairro|Idade|Sexo|
+
+## Output
+Each PDF file have four csv files. The ".pdf" ending is removed and a tN.csv is added, where N can be 1, 2, 3 or 4, one file for each table kind.
--- boletim_Paranaguá/parse_paranagua_pdf.py
+++ boletim_Paranaguá/parse_paranagua_pdf.py
@@ -8,6 +8,7 @@
 #   - Há em algum lugar o número de pacientes? Criar uma tabela gigantesca? Gravar diretamente no arquivo sem fazer uma tabela no programa?
 #
 
+# download all pdf that are not available
 def download_missing_pdf():
     import urllib
     from urllib import request
@@ -15,53 +16,35 @@ def download_missing_pdf():
     import re
     page = request.urlopen("http://www.paranagua.pr.gov.br/boletim-epidemiologico.php")
     html = page.read().decode('utf8')
-    # pattern = re.compile('\"[^\"]pdf\"')
     pattern = re.compile('href=[\'"]?downloads/boletins/([^\'">]+)')
     links = pattern.findall(html)
-    #'downloads/boletins/Boletim Epidemiológico 06-04-2020.pdf'
     for l in links:
         if not Path(l).is_file():
             try:
                 request.urlretrieve ("http://www.paranagua.pr.gov.br/downloads/boletins/" + urllib.request.pathname2url(l), filename=l)
             except:
                 print("File not found: {0}.".format("http://www.paranagua.pr.gov.br/downloads/boletins/" + l))
-    # write a file with pdf order
-    f = open('ordem.txt', 'w')
+    # write a file with all downloaded PDF file names order
+    f = open('order.txt', 'w')
     for l in links:
         f.write(l + '\n')
     f.close()
 
-def read_pdf():
-    from PyPDF4 import PdfFileReader
-    file = open('Boletim_CORONAVIRUS_11-07-2020.pdf', 'rb')
-    document = PdfFileReader(file)
-    document.numPages
-    document.numPages
-    document.documentInfo
-    document.pages[0]
-    document.pages[0].extractText()
-    p0 = document.pages[0]
-    p0.getContents()
-    p0.items
-    p0.items()
-    p0.extractText()
-    document.pages[0].extractText()
-    document.pages[0].extractText()
-    document.pages[1].extractText()
-    document.pages[3].extractText()
-    document.pages[7].extractText()
-    document.pages[6].extractText()
-    document.pages[0].extractText()
-    document.pages[3].extractText()
-    document.pages[4].extractText()
-    document.pages[5].extractText()
+# read order.txt and convert
+def convert_new_pdfs():
+    print('converting\n')
+    from pathlib import Path
+    with open('order.txt', 'r') as f:
+        for l in f.readlines():
+            l = l[:len(l)-1]
+            expected_csv_name = l[:len(l)-4] + 't1.csv'
+            print('Read line: ' + l)
+            if Path(l).is_file() and not Path(expected_csv_name).is_file():
+                print("Converting file" + l + '\n')
+                read_pdf(l)
 
-def read_tabula(pdf_name):
-    import tabula
-    output = pdf_name[0:(len(pdf_name)-4)]
-    tabula.convert_into(pdf_name, output, output_format="csv", pages="all")
-
-def read_camelot(pdf_name):
+# read a PDF file and generate the four csv files.
+def read_pdf(pdf_name):
     import camelot
     length = len(pdf_name)
     output = pdf_name[0:(len(pdf_name)-4)]
@@ -133,6 +116,5 @@ def read_camelot(pdf_name):
 # Boletim CORONAVÍRUS (02-06 <-> 19-07)-2020.pdf
 # T1, T2, T3, T4
 if __name__ == "__main__":
-#    download_missing_pdf():
-    pdf_name = 'Boletim CORONAVÍRUS 24-07-2020.pdf'
-
+    download_missing_pdf()
+    convert_new_pdfs()
